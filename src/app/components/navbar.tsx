3da5c85c87213f99a2d79ce5a066e6c119c3ce74
jatnerubia <jasonlerit077@gmail.com>
2024-09-15 14:26:58 +0800

    add github link

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,11 +1,21 @@
+import Link from "next/link"
 import { ModeToggle } from "./mode-toggle"
+import { Button } from "@/components/ui/button"
+import { GitHubLogoIcon } from "@radix-ui/react-icons"
 
 export const Navbar = () => {
   return (
     <header>
       <div className='container mx-auto h-14 flex justify-between items-center'>
         <span className='font-bold'>Fix My Grammar AI</span>
-        <ModeToggle />
+        <div className='flex items-center gap-2'>
+          <Button asChild variant='ghost'>
+            <Link href='https://github.com/jatnerubia/fix-my-grammar-ai-client'>
+              <GitHubLogoIcon />
+            </Link>
+          </Button>
+          <ModeToggle />
+        </div>
       </div>
     </header>
   )
